9ff2779c968dd601b6c1fa5d73b7c378396b43e1
DevanshuAgrawal <agrawald@goldmail.etsu.edu>
2018-10-22 23:13:21 -0400

    Add files where I started to modify for pretraining.

---
+++ project4/branch/autoencoder.py
@@ -0,0 +1,144 @@
+import time
+import tensorflow as tf
+import numpy as np
+
+class autoencoder:
+
+	def __init__(self, encoder_layers, blocks, lr=0.01, lr_decay=1, mbs=1, pred_mbs=None, seed=None):
+		self.encoder_layers = encoder_layers
+		self.blocks = blocks
+		self.lr = lr
+		self.lr_decay = lr_decay
+		self.mbs = mbs
+		self.pred_mbs = pred_mbs
+		if seed is not None: tf.set_random_seed(seed)
+		self.x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
+		self.build_autoencoder()
+
+	def build_encoder(self):
+		self.input_shapes_partial = []
+		self.input_shape_tensors = []
+		self.params = []
+		self.param_names = []
+		out = tf.identity(self.x)
+		for layer in self.encoder_layers:
+			self.input_shape_tensors.append(tf.shape(out))
+			out = layer(out)
+			self.input_shapes_partial.append(layer.input_shape)
+			self.params = self.params + layer.get_params()
+			self.param_names = self.param_names + layer.get_param_names()
+		self.encoded = tf.identity(out)
+
+	def build_decoder_layers(self):
+		first_param_layer = -1
+		for layer in self.encoder_layers:
+			first_param_layer += 1
+			if len(layer.get_param_names()) > 0:
+				break
+		self.decoder_layers = []
+		for i, (layer, output_shape) in enumerate(list(zip(self.encoder_layers, self.input_shape_tensors))[::-1]):
+			activation = tf.nn.sigmoid if i == first_param_layer else tf.nn.relu
+			self.decoder_layers.append( layer.decoder(output_shape, activation) )
+
+	def build_decoder(self):
+		out = tf.identity(self.encoded)
+		for layer in self.decoder_layers:
+			if layer is not None:
+				out = layer(out)
+		self.reconstruction = tf.identity(out)
+
+	def build_block_autoencoder(self, block):
+		self.block_input = tf.placeholder(tf.float32, shape=self.input_shapes_partial[block[0]])
+		out = tf.identity(self.block_input)
+		for layer in [self.encoder_layers[i] for i in block]:
+			out = layer(out)
+		self.block_encoded = tf.identity(out)
+		for layer in [self.decoder_layers[-1-i] for i in block[::-1]]:
+			if layer is not None:
+				out = layer(out)
+		loss = tf.nn.l2_loss(self.block_input-out)
+		self.pretrain_step = tf.train.AdamOptimizer(self.lr).minimize(loss)
+
+	def build_autoencoder(self):
+		self.build_encoder()
+		self.build_decoder_layers()
+		self.build_decoder()
+		self.loss = tf.nn.l2_loss(self.x - self.reconstruction)
+		self.train_step = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+
+	def start_session(self):
+		self.sess = tf.Session()
+		init = tf.global_variables_initializer()
+		self.sess.run(init)
+
+	def stop_session(self):
+		self.sess.close()
+
+	def pretrain(self, X_train, epochs=1):
+		n_train = X_train.shape[0]
+		n_batches = int(np.ceil(n_train/self.mbs))
+		mb_progress = int(0.1*n_batches)
+		X = np.copy(X_train)
+		for block in self.blocks:
+			print("Block ", block)
+			self.build_block_autoencoder(block)
+			for epoch in range(epochs):
+				print("Epoch ", epoch, " . . . ")
+				time_0 = time.time()
+				np.random.shuffle(X)
+				time_1 = time.time()
+				for k, i in enumerate(range(0,n_train,self.mbs)):
+#					if k%mb_progress == 0:
+#						print(k, "/", n_batches, " batches (", np.round(time.time()-#time_1, 5), " s)")
+#						time_1 = time.time()
+					X_batch = X[i:min(n_train,i+self.mbs)]
+					self.sess.run([self.pretrain_step], feed_dict={self.block_input: X_batch})
+			X = self.block_encode(X)
+		return
+
+	def block_encode(self, X):
+		n_test = X.shape[0]
+		batches = []
+		for i in range(0,n_test,self.pred_mbs):
+			X_batch = X[i:min(n_test,i+self.pred_mbs)]
+			batches.append( self.sess.run(self.block_encoded, feed_dict={self.block_input: X_batch}) )
+		return np.concatenate(tuple(batches), axis=0)
+
+
+	def train(self, X_train, X_test, epochs=1, early_stopping=None):
+		if self.pred_mbs is None: self.pred_mbs = X_test.shape[0]
+		n_train = X_train.shape[0]
+		n_batches = int(np.ceil(n_train/self.mbs))
+		mb_progress = int(0.1*n_batches)
+		indices = np.arange(X_train.shape[0])
+		losses = []
+		best_parameters = self.sess.run(self.params)
+		if early_stopping is None: early_stopping = epochs+1
+		for epoch in range(epochs):
+			print("Epoch ", epoch, " . . . ")
+			time_0 = time.time()
+			np.random.shuffle(indices)
+			X = X_train[indices]
+			if (1+epoch)%10 == 0: self.lr *= self.lr_decay
+			time_1 = time.time()
+			for k, i in enumerate(range(0,n_train,self.mbs)):
+#				if k%mb_progress == 0:
+#					print(k, "/", n_batches, " batches (", np.round(time.time()-#time_1, 5), " s)")
+#					time_1 = time.time()
+				X_batch = X[i:min(n_train,i+self.mbs)]
+				self.sess.run([self.train_step], feed_dict={self.x: X_batch})
+			losses.append(self.test_loss(X_test))
+			print("\t Test loss: ", np.round(losses[-1], 6), " (", np.round(time.time()-time_0, 5), " s)")
+			if 1+epoch >= 2 and losses[-1] < losses[-2]:
+				best_parameters = self.sess.run(self.params)
+			if 1+epoch >= early_stopping and np.argmin(losses[-early_stopping:])==0: break
+		best_parameters = {name: value for name, value in zip(self.param_names, best_parameters)}
+		return np.array(losses), best_parameters
+
+	def test_loss(self, X):
+		n_test = X.shape[0]
+		batch_losses = []
+		for i in range(0,n_test,self.pred_mbs):
+			X_batch = X[i:min(n_test,i+self.pred_mbs)]
+			batch_losses.append( self.sess.run(self.loss, feed_dict={self.x: X_batch}) )
+		return np.sum(batch_losses)/n_test
---
+++ project4/branch/layers.py
@@ -0,0 +1,396 @@
+from abc import ABCMeta, abstractmethod
+
+import tensorflow as tf
+import numpy as np
+
+
+class Layer(object, metaclass=ABCMeta):
+	"""
+
+	"""
+	def __init__(self):
+		pass
+
+	@abstractmethod
+	def call(self, *args, **kwargs):
+		raise NotImplementedError
+
+	def __call__(self, *args, **kwargs):
+		return self.call(*args, **kwargs)
+
+
+class Convolution2D(Layer):
+	"""
+
+	"""
+	def __init__(self,
+				 kernel_shape,
+				 kernel=None,
+				 bias=None,
+				 strides=(1, 1, 1, 1),
+				 padding='SAME',
+				 activation=None,
+				 scope=''):
+		Layer.__init__(self)
+
+		self.kernel_shape = kernel_shape
+		self.kernel = kernel
+		self.bias = bias
+		self.strides = strides
+		self.padding = padding
+		self.activation = activation
+		self.scope = scope
+
+	def build(self, input_tensor):
+		# store input shape
+		self.input_shape = input_tensor.get_shape().as_list()
+
+		# build kernel
+		if self.kernel is not None:
+			assert self.kernel.get_shape() == self.kernel_shape
+		else:
+			self.kernel = tf.Variable(tf.truncated_normal(self.kernel_shape, stddev=0.1), name='kernel')
+
+		# build bias
+		kernel_height, kernel_width, num_input_channels, num_output_channels = self.kernel.get_shape()
+		if self.bias:
+			assert self.bias.get_shape() == (num_output_channels, )
+		else:
+			self.bias = tf.Variable(tf.constant(0.1, shape=[num_output_channels]), name='bias')
+
+		# convolution
+		conv = tf.nn.conv2d(input_tensor, self.kernel, strides=self.strides, padding=self.padding)
+
+		# activation
+		if self.activation:
+			return self.activation(conv + self.bias)
+		return conv + self.bias
+
+	def call(self, input_tensor):
+		if self.scope:
+			with tf.variable_scope(self.scope) as scope:
+				return self.build(input_tensor)
+		else:
+			return self.build(input_tensor)
+
+	def decoder(self, output_shape, activation):
+		return DeConvolution2D(self.kernel_shape, output_shape, kernel=self.kernel, activation=activation, scope=self.scope+"_decoder")
+
+	def get_params(self):
+		return [self.kernel, self.bias]
+
+	def get_param_names(self):
+		return [self.scope+"_W", self.scope+"_b"]
+
+
+
+class DeConvolution2D(Layer):
+	"""
+
+	"""
+	def __init__(self,
+				 kernel_shape,
+				 output_shape,
+				 kernel=None,
+				 bias=None,
+				 strides=(1, 1, 1, 1),
+				 padding='SAME',
+				 activation=None,
+				 scope=''):
+		Layer.__init__(self)
+
+		self.kernel_shape = kernel_shape
+		self.output_shape = output_shape
+		self.kernel = kernel
+		self.bias = bias
+		self.strides = strides
+		self.padding = padding
+		self.activation = activation
+		self.scope = scope
+
+	def build(self, input_tensor):
+		# build kernel
+		if self.kernel:
+			assert self.kernel.get_shape() == self.kernel_shape
+		else:
+			self.kernel = tf.Variable(tf.truncated_normal(self.kernel_shape, stddev=0.1), name='kernel')
+
+		# build bias
+		window_height, window_width, num_output_channels, num_input_channels = self.kernel.get_shape()
+		if self.bias:
+			assert self.bias.get_shape() == (num_output_channels, )
+		else:
+			self.bias = tf.Variable(tf.constant(0.1, shape=[num_output_channels]), name='bias')
+
+		# convolution
+		deconv = tf.nn.conv2d_transpose(input_tensor,
+										self.kernel,
+										output_shape=self.output_shape,
+										strides=self.strides,
+										padding=self.padding)
+
+		# activation
+		if self.activation:
+			return self.activation(deconv + self.bias)
+		return deconv + self.bias
+
+	def call(self, input_tensor):
+		if self.scope:
+			with tf.variable_scope(self.scope) as scope:
+				return self.build(input_tensor)
+		else:
+			return self.build(input_tensor)
+
+
+class MaxPooling(Layer):
+	"""
+
+	"""
+	def __init__(self,
+				 kernel_shape,
+				 strides,
+				 padding,
+				 scope=''):
+		Layer.__init__(self)
+
+		self.kernel_shape = kernel_shape
+		self.strides = strides
+		self.padding = padding
+		self.scope = scope
+
+	def build(self, input_tensor):
+		# store input shape
+		self.input_shape = input_tensor.get_shape().as_list()
+
+		return tf.nn.max_pool(input_tensor, ksize=self.kernel_shape, strides=self.strides, padding=self.padding)
+
+	def call(self, input_tensor):
+		if self.scope:
+			with tf.variable_scope(self.scope) as scope:
+				return self.build(input_tensor)
+		else:
+			return self.build(input_tensor)
+
+	def decoder(self, output_shape, activation):
+		return UnPooling((2, 2), output_shape=output_shape, scope=self.scope+"_decoder")
+
+	def get_params(self):
+		return []
+
+	def get_param_names(self):
+		return []
+
+
+class UnPooling(Layer):
+	"""
+	Unpool a max-pooled layer.
+
+	Currently this method does not use the argmax information from the previous pooling layer.
+	Currently this method assumes that the size of the max-pooling filter is same as the strides.
+
+	Each entry in the pooled map would be replaced with an NxN kernel with the original entry in the upper left.
+	For example: a 1x2x2x1 map of
+
+		[[[[1], [2]],
+		  [[3], [4]]]]
+
+	could be unpooled to a 1x4x4x1 map of
+
+		[[[[ 1.], [ 0.], [ 2.], [ 0.]],
+		  [[ 0.], [ 0.], [ 0.], [ 0.]],
+		  [[ 3.], [ 0.], [ 4.], [ 0.]],
+		  [[ 0.], [ 0.], [ 0.], [ 0.]]]]
+	"""
+	def __init__(self,
+				 kernel_shape,
+				 output_shape,
+				 scope=''):
+		Layer.__init__(self)
+
+		self.kernel_shape = kernel_shape
+		self.output_shape = output_shape
+		self.scope = scope
+
+	def build(self, input_tensor):
+		num_channels = input_tensor.get_shape()[-1]
+		input_dtype_as_numpy = input_tensor.dtype.as_numpy_dtype()
+		kernel_rows, kernel_cols = self.kernel_shape
+
+		# build kernel
+		kernel_value = np.zeros((kernel_rows, kernel_cols, num_channels, num_channels), dtype=input_dtype_as_numpy)
+		kernel_value[0, 0, :, :] = np.eye(num_channels, num_channels)
+		kernel = tf.constant(kernel_value)
+
+		# do the un-pooling using conv2d_transpose
+		unpool = tf.nn.conv2d_transpose(input_tensor,
+										kernel,
+										output_shape=self.output_shape,
+										strides=(1, kernel_rows, kernel_cols, 1),
+										padding='VALID')
+		# TODO test!!!
+		return unpool
+
+	def call(self, input_tensor):
+		if self.scope:
+			with tf.variable_scope(self.scope) as scope:
+				return self.build(input_tensor)
+		else:
+			return self.build(input_tensor)
+
+
+class Unfold(Layer):
+	"""
+
+	"""
+	def __init__(self,
+				 scope=''):
+		Layer.__init__(self)
+
+		self.scope = scope
+
+	def build(self, input_tensor):
+		# store input shape
+		self.input_shape = input_tensor.get_shape().as_list()
+
+		num_batch, height, width, num_channels = input_tensor.get_shape()
+
+		return tf.reshape(input_tensor, [-1, (height * width * num_channels).value])
+
+	def call(self, input_tensor):
+		if self.scope:
+			with tf.variable_scope(self.scope) as scope:
+				return self.build(input_tensor)
+		else:
+			return self.build(input_tensor)
+
+	def decoder(self, output_shape, activation):
+		return Fold(output_shape, scope=self.scope+"_decoder")
+
+	def get_params(self):
+		return []
+
+	def get_param_names(self):
+		return []
+
+
+class Fold(Layer):
+	"""
+
+	"""
+	def __init__(self,
+				 fold_shape,
+				 scope=''):
+		Layer.__init__(self)
+
+		self.fold_shape = fold_shape
+		self.scope = scope
+
+	def build(self, input_tensor):
+		return tf.reshape(input_tensor, self.fold_shape)
+
+	def call(self, input_tensor):
+		if self.scope:
+			with tf.variable_scope(self.scope) as scope:
+				return self.build(input_tensor)
+		else:
+			return self.build(input_tensor)
+
+
+class FullyConnected(Layer):
+	"""
+
+	"""
+	def __init__(self,
+				 output_dim,
+				 weights=None,
+				 bias=None,
+				 activation=None,
+				 scope=''):
+		Layer.__init__(self)
+
+		self.output_dim = output_dim
+		self.weights = weights
+		self.bias = bias
+		self.activation = activation
+		self.scope = scope
+
+	def build(self, input_tensor):
+		# store input shape
+		self.input_shape = input_tensor.get_shape().as_list()
+
+		num_batch, input_dim = input_tensor.get_shape()
+
+		# build weights
+		if self.weights is not None:
+			assert self.weights.get_shape() == (input_dim.value, self.output_dim)
+		else:
+			self.weights = tf.Variable(tf.truncated_normal((input_dim.value, self.output_dim), stddev=0.1),
+									   name='weights')
+
+		# build bias
+		if self.bias:
+			assert self.bias.get_shape() == (self.output_dim, )
+		else:
+			self.bias = tf.Variable(tf.constant(0.1, shape=[self.output_dim]), name='bias')
+
+		# fully connected layer
+		fc = tf.matmul(input_tensor, self.weights) + self.bias
+
+		# activation
+		if self.activation:
+			return self.activation(fc)
+		return fc
+
+	def call(self, input_tensor):
+		if self.scope:
+			with tf.variable_scope(self.scope) as scope:
+				return self.build(input_tensor)
+		else:
+			return self.build(input_tensor)
+
+	def decoder(self, output_shape, activation):
+		output_dim, input_dim = self.weights.get_shape()
+		return FullyConnected(output_dim, weights=tf.transpose(self.weights), activation=activation, scope=self.scope+"_decoder")
+
+	def get_params(self):
+		return [self.weights, self.bias]
+
+	def get_param_names(self):
+		return [self.scope+"_W", self.scope+"_b"]
+
+
+class Corrupt(Layer):
+	"""
+
+	"""
+	def __init__(self,
+				 noise_stdev=0.1,
+				 scope=''):
+		Layer.__init__(self)
+
+		self.noise_stdev = noise_stdev
+		self.scope = scope
+
+	def build(self, input_tensor):
+		# store input shape
+		self.input_shape = input_tensor.get_shape().as_list()
+
+		noise = tf.random_normal(shape=tf.shape(input_tensor), mean=0.0, stddev=self.noise_stdev, dtype=tf.float32)
+		corrupt = tf.minimum(tf.maximum(input_tensor+noise, 0.0), 1.0)
+		return corrupt
+
+	def call(self, input_tensor):
+		if self.scope:
+			with tf.variable_scope(self.scope) as scope:
+				return self.build(input_tensor)
+		else:
+			return self.build(input_tensor)
+
+	def decoder(self, output_shape, activation):
+		return None
+
+	def get_params(self):
+		return []
+
+	def get_param_names(self):
+		return []
